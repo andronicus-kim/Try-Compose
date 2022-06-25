8bb9e538d53adabf5b98ec45d253eb8225017051
andronicus_kim <androkim.kim7@gmail.com>
2022-06-25 22:08:43 +0300

    adds custom font and styles text

--- textstyle/src/main/java/com/example/textstyle/MainActivity.kt
+++ textstyle/src/main/java/com/example/textstyle/MainActivity.kt
@@ -3,41 +3,63 @@ package com.example.textstyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.textstyle.ui.theme.TryComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fontFamily = FontFamily(
+            Font(R.font.quicksand_light, FontWeight.Light),
+            Font(R.font.quicksand_regular, FontWeight.Normal),
+            Font(R.font.quicksand_medium, FontWeight.Medium),
+            Font(R.font.quicksand_semibold, FontWeight.SemiBold),
+            Font(R.font.quicksand_bold, FontWeight.Bold)
+        )
         setContent {
-            TryComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black)
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack Compose")
+                    },
+                    color = Color.White,
+                    fontFamily = fontFamily,
+                    fontSize = 30.sp,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TryComposeTheme {
-        Greeting("Android")
     }
 }
