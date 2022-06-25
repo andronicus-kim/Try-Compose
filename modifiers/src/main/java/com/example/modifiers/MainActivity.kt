85a1f306cb4ad6d403d8d0f018a71591f3738fd5
andronicus_kim <androkim.kim7@gmail.com>
2022-06-25 12:29:34 +0300

    border modifier

--- modifiers/src/main/java/com/example/modifiers/MainActivity.kt
+++ modifiers/src/main/java/com/example/modifiers/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -31,10 +32,13 @@ class MainActivity : ComponentActivity() {
                     .fillMaxHeight(0.5f)
                     .width(300.dp)
                     .background(Color.Green)
-                    .padding(top = 10.dp, start = 16.dp)
+                    .padding(top = 10.dp)
+                    .border(5.dp,Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(10.dp)
             ){
-                Text(text = "Hello", modifier = Modifier
-                    .offset(0.dp,50.dp))
+                Text(text = "Hello")
                 Spacer(modifier = Modifier
                     .height(150.dp))
                 Text(text = "Word")
