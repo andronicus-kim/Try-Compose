6476b9e3031e82622d1442618c9a4942395b4073
andronicus_kim <androkim.kim7@gmail.com>
2022-06-25 11:09:20 +0300

    arrangment and alignment

--- rowscolumssizing/src/main/java/com/example/rowscolumssizing/MainActivity.kt
+++ rowscolumssizing/src/main/java/com/example/rowscolumssizing/MainActivity.kt
@@ -3,14 +3,22 @@ package com.example.rowscolumssizing
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Text(text = "Android")
                 Text(text = "Hello world!")
                 Text(text = "Hello!")
