548f39fac78616db1e5ec6f2c9932df6202cac45
andronicus_kim <androkim.kim7@gmail.com>
2022-06-25 12:04:51 +0300

    adds padding

--- modifiers/src/main/java/com/example/modifiers/MainActivity.kt
+++ modifiers/src/main/java/com/example/modifiers/MainActivity.kt
@@ -3,27 +3,35 @@ package com.example.modifiers
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.modifiers.ui.theme.TryComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TryComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .width(300.dp)
+                    .background(Color.Green)
+                    .padding(top = 10.dp, start = 16.dp)
+            ){
+                Text(text = "Hello")
+                Text(text = "Word")
             }
         }
     }
