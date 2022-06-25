e684f1ee111bd164d1a2b6714e1ca455be4c9081
andronicus_kim <androkim.kim7@gmail.com>
2022-06-25 10:53:50 +0300

    using column composable to align text

--- rowscolumssizing/src/main/java/com/example/rowscolumssizing/MainActivity.kt
+++ rowscolumssizing/src/main/java/com/example/rowscolumssizing/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.rowscolumssizing
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Android")
+            Column {
+                Text(text = "Android")
+                Text(text = "Hello world!")
+                Text(text = "Hello!")
+            }
         }
     }
 }
