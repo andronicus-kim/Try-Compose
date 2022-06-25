a887d0981cf0fe7a3ba20e10eee05ff2e3a37d38
andronicus_kim <androkim.kim7@gmail.com>
2022-06-25 10:25:09 +0300

    converts rowscolumnssizing to an application

--- rowscolumssizing/src/main/java/com/example/rowscolumssizing/MainActivity.kt
+++ rowscolumssizing/src/main/java/com/example/rowscolumssizing/MainActivity.kt
@@ -3,12 +3,13 @@ package com.example.rowscolumssizing
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Text(text = "Android")
         }
     }
 }
