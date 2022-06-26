6d095b5ba196454e88c487ce92ec16abd5373f78
andronicus_kim <androkim.kim7@gmail.com>
2022-06-26 17:39:20 +0300

    clean up

--- samples/src/main/java/com/example/samples/Circularprogress.kt
+++ samples/src/main/java/com/example/samples/Circularprogress.kt
@@ -8,29 +8,24 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.samples.ui.theme.TryComposeTheme
 
 class Circularprogress : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +34,7 @@ class Circularprogress : ComponentActivity() {
             Box(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center
-            ){
+            ) {
                 CircularProgressBar(percentage = 0.8f, number = 100)
             }
         }
@@ -55,18 +50,18 @@ class Circularprogress : ComponentActivity() {
         color: Color = Color.Green,
         animationDelay: Int = 0,
         animationDuration: Int = 1000
-    ){
+    ) {
         var animationPlayed by remember {
             mutableStateOf(false)
         }
         val animateFloat by animateFloatAsState(
-            targetValue = if(animationPlayed) percentage else 0f,
+            targetValue = if (animationPlayed) percentage else 0f,
             animationSpec = tween(
                 durationMillis = animationDuration,
                 delayMillis = animationDelay
             )
         )
-        LaunchedEffect(key1 = true){
+        LaunchedEffect(key1 = true) {
             animationPlayed = true
         }
         Box(modifier = Modifier.size(radius * 2f), contentAlignment = Alignment.Center) {
@@ -80,7 +75,7 @@ class Circularprogress : ComponentActivity() {
                 )
             }
             Text(
-                text = (number*percentage).toInt().toString(),
+                text = (number * percentage).toInt().toString(),
                 fontSize = fontSize,
                 fontWeight = FontWeight.Bold,
                 color = Color.Black
