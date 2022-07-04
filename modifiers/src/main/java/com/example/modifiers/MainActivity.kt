6b612e7a654d570f55c7b8347189d37705471acc
andronicus_kim <androkim.kim7@gmail.com>
2022-07-04 22:17:43 +0300

    removes unused imports

--- modifiers/src/main/java/com/example/modifiers/MainActivity.kt
+++ modifiers/src/main/java/com/example/modifiers/MainActivity.kt
@@ -8,20 +8,15 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.modifiers.ui.theme.TryComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,18 +39,5 @@ class MainActivity : ComponentActivity() {
                 Text(text = "Word")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TryComposeTheme {
-        Greeting("Android")
     }
 }
